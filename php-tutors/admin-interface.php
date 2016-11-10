abf0f09f6ad1e5cfcbde63743136cca026cb1a11
Sophie Polson <sep45@duke.edu>
2016-11-10 02:29:41 +0000

    more pages

--- php-tutors/admin-interface.php
+++ php-tutors/admin-interface.php
@@ -3,6 +3,8 @@
 <body>
 <h1>Administrator Homepage</h1>
 
+<div class="topcorner"><a href="index.php">Back to Homepage</a></div>
+
 <a href="admin-upload.php">Upload a Qualtrics CSV</a>
 
 <br><br>
@@ -10,6 +12,26 @@
 <a href="individual-request.php">Add an individual request</a>
 
 <br><br>
+
+<a href="edit-request.php">Edit a request</a>
+
+<br><br>
+
+<a href="submit-match.php">Submit a student match</a>
+
+<br><br>
+
+<a href="delete-all.php">Delete all requests</a>
+
+<br><br>
+
+<style type="text/css">
+ .topcorner{
+   position:absolute;
+   top:5;
+   right:5;
+  }
+</style>
 
 </body>
 </html>
