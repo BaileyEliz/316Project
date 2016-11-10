aa4fabef394b721792eec73e7c33d58da8ceeeb5
Justin Bergkamp <jtb43@duke.edu>
2016-11-10 18:41:36 +0000

    Added new SQL queries, Updated milestone1

---
+++ test-production.sql
@@ -0,0 +1,36 @@
+-- SQL Queries
+
+-- Returns all potential matches: where request times match tutor availability 
+
+SELECT tutor_id, teacher_email, Request.day, Request.start_time, Request.end_time 
+FROM Request, TutorAvailable 
+WHERE TutorAvailable.day = Request.day and TutorAvailable.start_time <= Request.start_time and TutorAvailable.end_time >= Request.end_time;
+
+SELECT name,day, start_time, end_time 
+FROM TutorAvailable, TutorInfo
+WHERE TutorAvailable.tutor_id = 'sep45' and TutorAvailable.tutor_id = TutorInfo.tutor_id;
+
+-- Returns a list of teachers at sites 
+
+SELECT Site.name, Teacher.name
+FROM Site, Teacher
+WHERE Site.name = Teacher.site_name
+ORDER BY Site.name; 
+
+-- Returns requests that fit the schedule/requirements for a single tutor identified by their tutor_id
+
+SELECT tutor_id, teacher_email, Request.day, Request.start_time, Request.end_time
+FROM Request, TutorAvailable 
+WHERE TutorAvailable.day = Request.day and TutorAvailable.start_time <= Request.start_time and TutorAvailable.end_time >= Request.end_time and tutor_id = 'sep45'; 
+
+-- Returns the site and name and request number of all teachers with requests
+
+SELECT SITE.name, Teacher.name
+FROM Site, Request, Teacher
+WHERE Request.teacher_email  = Teacher.email and Teacher.site_name = Site.name;
+
+
+-- Joins the request with the teacher requesting it
+SELECT * 
+FROM Request, Teacher 
+WHERE teacher_email = email;
