319d2231feede86f666b955253f63b2a86590361
Bailey Wall <bew21@duke.edu>
2016-11-25 07:03:10 +0000

    tutor slot placement

--- php-tutors/pick-a-student-bs.php
+++ php-tutors/pick-a-student-bs.php
@@ -27,7 +27,7 @@
 
 <?php
 
-try {
+  try {
     include("pdo-tutors.php");
     $dbh = dbconnect();
   } catch (PDOException $e) {
@@ -84,38 +84,131 @@ try {
       <div class="row"><div class="col-md-12"><div class="time">9:30</div></div></div>
       <div class="row"><div class="col-md-12"><div class="time">10:00</div></div></div>
       <div class="row"><div class="col-md-12"><div class="time">10:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">11:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">11:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">12:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">12:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">1:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">1:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">2:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">2:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">3:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">3:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">4:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">4:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">5:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">5:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">6:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">6:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">7:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">7:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">8:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">8:30</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">9:00</div></div></div>
+      <div class="row"><div class="col-md-12"><div class="time">9:30</div></div></div>
     </div>
     <div class="col-md-2">Monday
       <div class="row">
         <div class="col-md-12">
-          <div class="monday-contents">Here's monday</div>
+          <div class="monday-contents"></div>
         </div>
       </div>
     </div>
     <div class="col-md-2">Tuesday
       <div class="row">
-        <div class="col-md-12">Here's monday</div>
+        <div class="col-md-12">
+          <div class="tuesday-contents"></div>
+        </div>
       </div>
     </div>
     <div class="col-md-2">Wednesday
       <div class="row">
-        <div class="col-md-12">Here's monday</div>
+        <div class="col-md-12">
+          <div class="wednesday-contents"></div>
+        </div>
       </div>
     </div>
     <div class="col-md-2">Thursday
       <div class="row">
-        <div class="col-md-12">Here's monday</div>
+        <div class="col-md-12">
+          <div class="thursday-contents"></div>
+        </div>
       </div>
     </div>
     <div class="col-md-2">Friday
       <div class="row">
-        <div class="col-md-12">Here's monday</div>
+        <div class="col-md-12">
+          <div class="friday-contents"></div>
+        </div>
       </div>
     </div>
   </div>
 </div>
 
-  <?php 
+<?php
+
+    function max_overlap($times_array, $start_time, $end_time) {
+      $maximum = 0;
+      $start_hour = intval(substr($start_time, 0, 2));
+      $start_minute = intval(substr($start_time, 3, 5));
+      $end_hour = intval(substr($end_time, 0, 2));
+      $end_minute = intval(substr($end_time, 3, 5));
+      $temp_minute = $start_minute;
+      for ($i = $start_hour; $i <= $end_hour; $i++) {
+        for ($j = $temp_minute; (($j < $end_minute and $i == $end_hour) or ($j < 60 and $i < $end_hour)); $j = $j + 5) {
+          if (count($times_array[$i][$j]) > $maximum) {
+            $maximum = count($times_array[$i][$j]);
+          }
+        } 
+        $temp_minute = 0;
+      }
+      if ($maximum == 0) {
+        $maximum = 1;
+      }
+      return $maximum;
+    }
+
+    function time_print($time) {
+      $h = intval(substr($time, 0, 2));
+      $m = intval(substr($time, 3, 5));
+      if ($m == 0) {
+        $m = "00";
+      }
+
+      if ($h < 12) {
+        return $h . ":" . $m . " AM";
+      }
+      else if ($h < 13) {
+        return $h . ":" . $m . " PM";
+      }
+      else {
+        return ($h - 12) . ":" . $m . " PM";
+      }
+    }
+
+    function minutes_different($start_time, $end_time) {
+      $start_hour = intval(substr($start_time, 0, 2));
+      $start_minute = intval(substr($start_time, 3, 5));
+      $end_hour = intval(substr($end_time, 0, 2));
+      $end_minute = intval(substr($end_time, 3, 5));
+
+      $temp_hour = $start_hour;
+      $minutes = 0;
+
+      if ($start_minute > 0) {
+        $minutes += (60 - $start_minute);
+        $temp_hour += 1;
+      }
+
+      $minutes += 60 * ($end_hour - $temp_hour);
+      $minutes += $end_minute;
+      return $minutes;
+
+    }
+
+    function top_margin($time) {
+      return minutes_different("08:00:00", $time);
+    }
 
     function html_print($index, $array) {
       if (count($array) == 0) {
@@ -123,7 +216,7 @@ try {
       }
       $request_id = $array['request_id'];
       $build = "<div id ='option_" . $index . "'>";
-      $build .= $array['name'] . "</br>" . $array['site_name'] . "</br>" . $array['start_time'] . "</br>" . $array['end_time'] . "</br>";
+      $build .= $array['name'] . "</br>" . $array['site_name'] . "</br>" . time_print($array['start_time']) . "</br>" . time_print($array['end_time']) . "</br>";
       $build .= "<form method=\"post\" action=\"request-details.php\">";
       $build .= "<input type=\"hidden\" name=\"request_id\" value=\"" . $request_id . "\">";
       $build .= "<input type=\"submit\" value=\"details\" id=\"link_button\">";
@@ -132,29 +225,56 @@ try {
       return $build;
     }
 
-    function css_print($index, $array) {
-
-      $h = intval(substr($array['start_time'], 0, 2));
-      $m = intval(substr($array['start_time'], 3, 5));
+    function css_print($times_array, $index, $array, $day) {
+      
       $d = $array['day'];
 
       $build = "<script type='text/javascript'>";
-      $build .= "var styles = {'background-color': 'red', 'position': 'relative', 'bottom':'" . 0 . "px', 'left':'" . 0 . "px'};";
+      $build .= "var styles = {
+        'background-color': 'red', 
+        'position': 'absolute', 
+        'top':'" . (6 * top_margin($array['start_time'])) . "px', 
+        'float':'left',
+        'height':'" . (6 * minutes_different($array['start_time'], $array['end_time'])) ."px',
+        'width':'" . ((1 / max_overlap($times_array, $array['start_time'], $array['end_time'])) * 100) . "%'};";
       $build .= "$('#option_" . $index . "').css(styles);";
+      $build .= "$('." . $day . "-contents').append($('#option_" . $index . "'));";
       $build .= "</script>";
       return $build;
     }
 
-    for ($i = 0; $i < count($values); $i++){
-      echo html_print($i, $values[$i]);
-      echo css_print($i, $values[$i]);
+    $monday_times = array();
+
+    for ($x = 0; $x < count($monday); $x++) {
+    $session = $monday[$x];
+    $start_hour = intval(substr($session['start_time'], 0, 2));
+    $start_minute = intval(substr($session['start_time'], 3, 5));
+    $end_hour = intval(substr($session['end_time'], 0, 2));
+    $end_minute = intval(substr($session['end_time'], 3, 5));
+    $temp_minute = $start_minute;
+    for ($i = $start_hour; $i <= $end_hour; $i++) {
+      for ($j = $temp_minute; (($j < $end_minute and $i == $end_hour) or ($j < 60 and $i < $end_hour)); $j = $j + 5) {
+        $monday_times[$i][$j][] = $x;
+      }
+      $temp_minute = 0;
+    }
+  }
+
+    $weekdaze = array($monday, $tuesday, $wednesday, $thursday, $friday);
+    $weekdaze_names = array("monday", "tuesday", "wednesday", "thursday", "friday");
+    //global $monday_times;
+
+    for ($x = 0; $x < count($weekdaze); $x++) {
+      $day_array = $weekdaze[$x];
+      $day_name = $weekdaze_names[$x];
+      for ($i = 0; $i < count($day_array); $i++){
+        global $monday_times;
+        echo html_print((($x * 100) + $i), $day_array[$i]);
+        echo css_print($monday_times, (($x * 100) + $i), $day_array[$i], $day_name);
+      }
     }
 
   ?>
-
-<script>
-  $(".monday-contents").append($("#option_0"));
-</script>
 
 Go <a href='all-students.php'>back</a>.
 </body>
