80d2502b9ac4d86352daf003b595ed6460eed1ec
Sophie Polson <sep45@duke.edu>
2016-12-07 22:26:52 +0000

    fixed upload, still some questions

--- php-tutors/format-requests.php
+++ php-tutors/format-requests.php
@@ -16,9 +16,9 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+        <?php include_once('admin_navbar.php'); ?>
   </head>
 <body>
-<div class="text-right"><a href="admin_home.php">Back to Admin Homepage</a></div>
 <?php
   try {
     // Including connection info (including database password) from outside
@@ -180,7 +180,7 @@
           $statement2values[] = intval($num_tutors);
           $statement2values[] = $language;
           $statement2values[] = $description;
-          $statement2values[] = "FALSE";
+          $statement2values[] = 0;
         try {
           $statement2->execute($statement2values);
         }
@@ -198,13 +198,4 @@ fclose($handle);
 ?>
 
 </body>
-
-<style type="text/css">
- .topcorner{
-   position:absolute;
-   top:5;
-   right:5;
-  }
-</style>
-
 </html>
